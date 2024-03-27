a9afceac0b3087302eb3b47d55a395c3688541da
Tsegelnik Nikita <tsegelnik@jinr.ru>
2024-03-27 15:34:37 +0300

    adding parameters plot, small update of the test

--- test/dgf_statistics/minimizer/test_IMinuitMinimizer.py
+++ test/dgf_statistics/minimizer/test_IMinuitMinimizer.py
@@ -1,5 +1,6 @@
 from math import sqrt
 
+from matplotlib import pyplot as plt  # fmt:skip
 from numpy import allclose, linspace
 from pytest import mark
 from scipy.stats import norm
@@ -15,7 +16,7 @@ from dgf_statistics.CNPStat import CNPStat
 from dgf_statistics.minimizer.iminuitminimizer import IMinuitMinimizer
 from dgf_statistics.MonteCarlo import MonteCarlo
 
-_NevScale = 10000
+_NevScale = 100000
 
 
 class NormalPDF(OneToOneNode):
@@ -38,7 +39,7 @@ class NormalPDF(OneToOneNode):
 @mark.parametrize("mu", (-1.531, 2.097))
 @mark.parametrize("sigma", (0.567, 1.503))
 def test_IMinuitMinimizer_normal(mu, sigma, testname):
-    size = 1001
+    size = 101
     x = linspace(-8, 8, size)
 
     # start values of the fitting
@@ -85,7 +86,7 @@ def test_IMinuitMinimizer_normal(mu, sigma, testname):
     minimizer = IMinuitMinimizer(statistic=chi.outputs[0], parameters=[parmu, parsigma], verbose=False)
     res = minimizer.fit()
 
-    atol = 5.0 / sqrt(_NevScale)
+    atol = 2.0 / sqrt(_NevScale)
     assert allclose(res["x"], [mu, sigma], rtol=0, atol=atol)
     assert allclose(res["covariance"], minimizer.calculate_covariance(), rtol=0, atol=1e-10)
 
@@ -93,20 +94,37 @@ def test_IMinuitMinimizer_normal(mu, sigma, testname):
     errors = minimizer.profile_errors()
     names = res["names"]
     assert errors["names"] == names
-    assert all(abs(err) < atol for errs in errors["errors"] for err in errs)
-    assert all(errors["errorsdict"][key] == errors["errors"][i] for i, key in enumerate(names))
+    errs = errors["errors"]
+    assert all(abs(err) < atol for errParam in errs for err in errParam)
+    assert all(errors["errorsdict"][key] == errs[i] for i, key in enumerate(names))
     for name in names:
         for key in ("is_valid", "lower_valid", "upper_valid"):
             assert errors["errors_profile_status"][name][key]
         assert errors["errors_profile_status"][name]["message"] == ""
 
     # save plot and graph
-    draw(x, mc, model, modelfit, f"output/{testname}-plot.png")
+    draw_params(res["x"], mu, sigma, minimizer, f"output/{testname}-params.png")
+    draw_fit(x, mc, model, modelfit, f"output/{testname}-plot.png")
     savegraph(graph, f"output/{testname}.png")
 
 
-def draw(x, mc, model, modelfit, figname):
-    from matplotlib import pyplot as plt  # fmt:skip
+def draw_params(res, mu, sigma, minimizer, figname):
+    _, ax = plt.subplots()
+    for cl in (1, 2, 3):
+        contours = minimizer._minimizer.mncontour(0, 1, cl=cl)
+        ax.plot(contours[:, 0], contours[:, 1], label=f"{cl}σ")
+    ax.scatter(*[mu, sigma], label="true")
+    ax.scatter(*res, label="fit")
+    plt.xlabel("mu")
+    plt.ylabel("sigma")
+    plt.legend()
+    plt.xlim([mu - 0.01, mu + 0.01])
+    plt.ylim([sigma - 0.01, sigma + 0.01])
+    plt.savefig(figname)
+    plt.close()
+
+
+def draw_fit(x, mc, model, modelfit, figname):
     def _create_fig():
         result = plt.subplot(111)
         result.minorticks_on()
@@ -116,24 +134,9 @@ def draw(x, mc, model, modelfit, figname):
         return result
 
     ax = _create_fig()
-    plot_array_1d(
-        mc.outputs[0].data,
-        meshes=x,
-        color="black",
-        label="MC",
-    )
-    plot_array_1d(
-        model.data,
-        meshes=x,
-        linestyle="--",
-        label="model",
-    )
-    plot_array_1d(
-        modelfit.data,
-        meshes=x,
-        linestyle="--",
-        label="fit",
-    )
+    plot_array_1d(mc.outputs[0].data, meshes=x, color="black", label="MC")
+    plot_array_1d(model.data, meshes=x, linestyle="--", label="model")
+    plot_array_1d(modelfit.data, meshes=x, linestyle="--", label="fit")
     ax.legend()
     plt.savefig(figname)
     plt.close()
