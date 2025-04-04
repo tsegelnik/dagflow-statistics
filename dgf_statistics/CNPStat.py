14d11f782190a42329b7ec21a344332c54958e4c
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2025-04-04 11:50:22 +0300

    feat: add CNP stat variance mode to existing uncertainty
    improve performance (expect), add CNPStat unit test

--- dgf_statistics/CNPStat.py
+++ dgf_statistics/CNPStat.py
@@ -3,15 +3,16 @@ from __future__ import annotations
 from math import sqrt
 from typing import TYPE_CHECKING
 
-from dagflow.lib.abstract import BlockToOneNode
+from numba import njit
+
 from dagflow.core.type_functions import (
     AllPositionals,
     check_dimension_of_inputs,
-    check_inputs_number_is_divisible_by_N,
     check_inputs_have_same_shape,
+    check_inputs_number_is_divisible_by_N,
     copy_from_inputs_to_outputs,
 )
-from numba import njit
+from dagflow.lib.abstract import BlockToOneNode
 
 if TYPE_CHECKING:
     from numpy import double
@@ -19,7 +20,7 @@ if TYPE_CHECKING:
 
 
 @njit(cache=True)
-def _cnp(
+def _cnp_uncertainty(
     data: NDArray[double],
     theory: NDArray[double],
     result: NDArray[double],
@@ -29,11 +30,20 @@ def _cnp(
         result[i] = coeff / sqrt(1.0 / data[i] + 2.0 / theory[i])
 
 
+@njit(cache=True)
+def _cnp_variance(
+    data: NDArray[double],
+    theory: NDArray[double],
+    result: NDArray[double],
+) -> None:
+    for i in range(len(data)):
+        result[i] = 3.0 / (1.0 / data[i] + 2.0 / theory[i])
+
+
 class CNPStat(BlockToOneNode):
-    r"""
-    Combined Neyman–Pearson statistic uncertainty:
-        errors = sqrt(3) / sqrt(1/dataᵢ+2/theoryᵢ), so if we connect it to `Chi2` node we get
-        χ² = (1/3) Σᵢ [(1/dataᵢ+2/theoryᵢ)·(theoryᵢ-dataᵢ)²]
+    r"""Combined Neyman–Pearson statistic uncertainty: errors = sqrt(3) /
+    sqrt(1/dataᵢ+2/theoryᵢ), so if we connect it to `Chi2` node we get χ² =
+    (1/3) Σᵢ [(1/dataᵢ+2/theoryᵢ)·(theoryᵢ-dataᵢ)²]
 
     inputs:
         `i`: data 1d array
@@ -47,34 +57,56 @@ class CNPStat(BlockToOneNode):
 
     __slots__ = ()
 
-    def __init__(self, name, *args, **kwargs):
+    def __init__(
+        self,
+        name,
+        *args,
+        mode: Literal["uncertainty", "variance"] = "uncertainty",
+        **kwargs,
+    ):
         super().__init__(name, *args, **kwargs)
+        assert mode in {"variance", "uncertainty"}
         self.labels.setdefaults(
             {
-                "text": r"CNP stat. uncertainty",
-                "plottitle": r"CNP stat. uncertainty",
-                "latex": r"CNP stat. uncertainty",
-                "axis": r"CNP stat. uncertainty",
-                "mark": r"σ(CNP)",
+                "text": f"CNP stat. {mode}",
+                "plottitle": f"CNP stat. {mode}",
+                "latex": f"CNP stat. {mode}",
+                "axis": f"CNP stat. {mode}",
+                "mark": "σ(CNP)",
             }
         )
+
+        self._functions_dict.update({
+            "uncertainty": self._cnp_uncertainty,
+            "variance": self._cnp_variance
+        })
+        self.function = self._functions_dict[mode]
 
     @staticmethod
     def _input_names() -> tuple[str, ...]:
         return "data", "theory"
 
-    def _function(self) -> None:
+    def _cnp_uncertainty(self) -> None:
         i = 0
-        while i < self.inputs.len_pos():
-            _cnp(
-                self.inputs[i].data,
-                self.inputs[i + 1].data,
-                self.outputs[i // 2]._data,
+        for i, output_data in enumerate(self._output_data):
+            _cnp_uncertainty(
+                self._input_data[2*i],
+                self._input_data[2*i+1],
+                output_data
+            )
+
+    def _cnp_variance(self) -> None:
+        i = 0
+        for i, output_data in enumerate(self._output_data):
+            _cnp_variance(
+                self._input_data[2*i],
+                self._input_data[2*i+1],
+                output_data
             )
             i += 2
 
     def _type_function(self) -> None:
-        """A output takes this function to determine the dtype and shape"""
+        """A output takes this function to determine the dtype and shape."""
         check_inputs_number_is_divisible_by_N(self, 2)
         check_dimension_of_inputs(self, AllPositionals, 1)
         i = 0
--- tests/dgf_statistics/test_Chi2.py
+++ tests/dgf_statistics/test_Chi2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from numpy import allclose, arange, array, diag, finfo, matmul
+from numpy import allclose, arange, array, diag, finfo, matmul, sqrt
 from numpy.linalg import cholesky, inv
 from pytest import mark
 
@@ -101,7 +101,9 @@ def test_Chi2CNPStat_v01(debug_graph, testname):
     with Graph(close_on_exit=True, debug=debug_graph) as graph:
         data = Array("data", dataa, mark="Data")
         theory = Array("theory", theorya, mark="Theory")
+
         cnp = CNPStat(name="cnp", label="CNP stat. uncertainty")
+
         (data, theory) >> cnp
         chi2 = Chi2(name="chi2", label="chi2")
         data >> chi2("data")
@@ -109,6 +111,35 @@ def test_Chi2CNPStat_v01(debug_graph, testname):
         cnp.outputs[0] >> chi2("errors")
     res = chi2.outputs["result"].data[0]
     res_expected = ((1.0 / dataa + 2.0 / theorya) * (theorya - dataa) ** 2).sum() / 3.0
+    assert allclose(res, res_expected, atol=finfo("d").resolution)
+
+    savegraph(graph, f"output/{testname}.png")
+
+
+@mark.parametrize("mode", (None, "uncertainty", "variance"))
+def test_Chi2CNPStat_v01(mode: str | None, debug_graph, testname):
+    n = 10
+    start = 10
+    offset = 1.0
+    dataa = arange(start, start + n, dtype="d") + 1
+    theorya = dataa + offset
+
+    with Graph(close_on_exit=True, debug=debug_graph) as graph:
+        data = Array("data", dataa, mark="Data")
+        theory = Array("theory", theorya, mark="Theory")
+
+        if mode:
+            cnp = CNPStat(name="cnp", mode=mode, label=f"CNP stat. {mode}")
+        else:
+            cnp = CNPStat(name="cnp", label=f"CNP stat. {mode}")
+        (data, theory) >> cnp
+
+    res = cnp.outputs["result"].data
+    if mode == "uncertainty" or mode is None:
+        res_expected = sqrt(3.0 / (1 / dataa + 2 / theorya))
+    elif mode == "variance":
+        res_expected = 3.0 / (1 / dataa + 2 / theorya)
+
     assert allclose(res, res_expected, atol=finfo("d").resolution)
 
     savegraph(graph, f"output/{testname}.png")
