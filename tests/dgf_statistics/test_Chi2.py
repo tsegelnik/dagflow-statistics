d991ccafbc16bb04a8565ed22b4403c153532587
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2025-04-08 13:06:46 +0300

    chore: separate test for CNPStat

--- tests/dgf_statistics/test_Chi2.py
+++ tests/dgf_statistics/test_Chi2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-from numpy import allclose, arange, array, diag, finfo, matmul, sqrt
+
+from numpy import allclose, arange, array, diag, finfo, matmul
 from numpy.linalg import cholesky, inv
 from pytest import mark
 
@@ -7,7 +8,6 @@ from dagflow.core.graph import Graph
 from dagflow.lib.common import Array
 from dagflow.plot.graphviz import savegraph
 from dgf_statistics.Chi2 import Chi2
-from dgf_statistics.CNPStat import CNPStat
 
 
 def test_Chi2_01(debug_graph, testname):
@@ -87,59 +87,5 @@ def test_Chi2_03(duplicate: bool, debug_graph, testname):
 
     assert allclose(res, truth1, rtol=0, atol=finfo("d").resolution)
     assert allclose(res, truth2, rtol=0, atol=finfo("d").resolution)
-
-    savegraph(graph, f"output/{testname}.png")
-
-
-def test_Chi2CNPStat_v01(debug_graph, testname):
-    n = 10
-    start = 10
-    offset = 1.0
-    dataa = arange(start, start + n, dtype="d") + 1
-    theorya = dataa + offset
-
-    with Graph(close_on_exit=True, debug=debug_graph) as graph:
-        data = Array("data", dataa, mark="Data", mode="fill")
-        theory = Array("theory", theorya, mark="Theory", mode="fill")
-
-        cnp = CNPStat(name="cnp", label="CNP stat. uncertainty")
-
-        (data, theory) >> cnp
-        chi2 = Chi2(name="chi2", label="chi2")
-        data >> chi2("data")
-        theory >> chi2("theory")
-        cnp.outputs[0] >> chi2("errors")
-    res = chi2.outputs["result"].data[0]
-    res_expected = ((1.0 / dataa + 2.0 / theorya) * (theorya - dataa) ** 2).sum() / 3.0
-    assert allclose(res, res_expected, atol=finfo("d").resolution)
-
-    savegraph(graph, f"output/{testname}.png")
-
-
-@mark.parametrize("mode", (None, "uncertainty", "variance"))
-def test_Chi2CNPStat_v01(mode: str | None, debug_graph, testname):
-    n = 10
-    start = 10
-    offset = 1.0
-    dataa = arange(start, start + n, dtype="d") + 1
-    theorya = dataa + offset
-
-    with Graph(close_on_exit=True, debug=debug_graph) as graph:
-        data = Array("data", dataa, mark="Data", mode="fill")
-        theory = Array("theory", theorya, mark="Theory", mode="fill")
-
-        if mode:
-            cnp = CNPStat(name="cnp", mode=mode, label=f"CNP stat. {mode}")
-        else:
-            cnp = CNPStat(name="cnp", label=f"CNP stat. {mode}")
-        (data, theory) >> cnp
-
-    res = cnp.outputs["result"].data
-    if mode == "uncertainty" or mode is None:
-        res_expected = sqrt(3.0 / (1 / dataa + 2 / theorya))
-    elif mode == "variance":
-        res_expected = 3.0 / (1 / dataa + 2 / theorya)
-
-    assert allclose(res, res_expected, atol=finfo("d").resolution)
 
     savegraph(graph, f"output/{testname}.png")
