4743dca61ca465a5b017b3b707f9422c560366f1
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2025-04-18 17:42:02 +0300

    feat: verbose minimizable, another method

--- dgf_statistics/minimizer/minimizable.py
+++ dgf_statistics/minimizer/minimizable.py
@@ -3,10 +3,12 @@ from __future__ import annotations
 from collections.abc import Sequence
 from typing import TYPE_CHECKING
 
+from numpy import full_like, where
+
 from dagflow.core.exception import InitializationError
-from dagflow.tools.logger import Logger, get_logger
 from dagflow.core.output import Output
 from dagflow.parameters import Parameter
+from dagflow.tools.logger import Logger, get_logger
 
 if TYPE_CHECKING:
     from typing import Callable
@@ -15,7 +17,20 @@ if TYPE_CHECKING:
 
 
 class Minimizable:
-    __slots__ = ("_statistic", "_parameters", "_verbose", "_functions", "_function", "_ncall", "_logger")
+    __slots__ = (
+        "_statistic",
+        "_parameters",
+        "_verbose",
+        "_functions",
+        "_function",
+        "_ncall",
+        "_nstep",
+        "_previous_fcn_step",
+        "_previous_fcn_call",
+        "_previous_parameters_step",
+        "_previous_parameters_call",
+        "_logger",
+    )
 
     _statistic: Output
     _parameters: list[Parameter]
@@ -23,6 +38,11 @@ class Minimizable:
     _functions: dict
     _function: Callable
     _ncall: int
+    _nstep: int
+    _previous_fcn_step: float | None
+    _previous_fcn_call: float | None
+    _previous_parameters_step: NDArray | None
+    _previous_parameters_call: NDArray | None
     _logger: Logger
 
     def __init__(
@@ -51,65 +71,99 @@ class Minimizable:
         if isinstance(logger, Logger):
             self._logger = logger
         elif logger is not None:
-            raise InitializationError(f"Cannot initialize a Minimizable class with logger={logger}")
+            raise InitializationError(
+                f"Cannot initialize a Minimizable class with logger={logger}"
+            )
         else:
             self._logger = get_logger()
 
         self._verbose = verbose
-        self._functions = {"verbose": self._function_verbose, "default": self._function_default}
+        self._functions = {
+            "verbose": self._function_verbose,
+            "default": self._function_default,
+        }
         self._function = self._functions["verbose" if verbose else "default"]
         self._ncall = 0
+        self._nstep = 0
+        self._previous_fcn_step = None
+        self._previous_fcn_call = None
+        self._previous_parameters_step = None
+        self._previous_parameters_call = None
 
     def append_par(self, par: Parameter) -> None:
         if not isinstance(par, Parameter):
-            raise RuntimeError(f"par must be a Parameter, but given {par=}, {type(par)=}!")
+            raise RuntimeError(
+                f"par must be a Parameter, but given {par=}, {type(par)=}!"
+            )
         self._parameters.append(par)
 
-    def _function_default(self, values: "NDArray") -> float:
+    def _function_default(self, values: NDArray) -> float:
         for param, val in zip(self._parameters, values):
             param.value = val
         self._ncall += 1
         return self._statistic.data[0]
 
-    def _function_verbose(self, values: "NDArray") -> float:
-        n_modified_pars = 0
-        n_pars = len(self._parameters)
-        modified_pars = []
-        modified_pars_numbers = []
-        for i, (param, val) in enumerate(zip(self._parameters, values)):
-            if (param_modified:=(param.value!=val)):
-                n_modified_pars+=1
+    def _function_verbose(self, values: NDArray) -> float:
+        n_parameters = len(self._parameters)
+        if self._previous_parameters_step is None:
+            parameters_change_step = full_like(values, 0.0)
+            modified_parameters_count = 0
+            call_type = "initial"
+            self._nstep+=1
+            modified_parameter_numbers = ""
+            modified_parameters = ""
+        else:
+            parameters_change_step = values-self._previous_parameters_step
+            modified_parameters_idx = where(parameters_change_step)[0]
+            modified_parameters_count = modified_parameters_idx.size
 
-                if n_modified_pars<3:
-                    modified_pars.append(param.name)
-                    modified_pars_numbers.append(i)
+            if n_parameters>1:
+                if modified_parameters_count==n_parameters:
+                    call_type = "step"
+                    self._nstep+=1
+                elif modified_parameters_count==1:
+                    call_type = "derivative"
+                else:
+                    call_type = "call type determination failed"
+            else:
+                call_type = "undefined"
+
+            if modified_parameters_idx.size==n_parameters:
+                modified_parameter_numbers = "all"
+                modified_parameters = "all"
+
+            if (modified_parameters_truncated:=modified_parameters_count>2):
+                modified_parameters_idx = modified_parameters_idx[:2]
+
+            modified_parameter_numbers = f"{', '.join(map(str, modified_parameters_idx))}"
+            modified_parameters = f"{', '.join(self._parameters[i].name for i in modified_parameters_idx)}"
+            if modified_parameters_truncated:
+                modified_parameter_numbers = f"{modified_parameter_numbers}, ..."
+                modified_parameters = f"{modified_parameters}, ..."
+
+        for i, (param, val, change) in enumerate(zip(self._parameters, values, parameters_change_step)):
             param.value = val
-            self._logger.info(f"{param!s}{param_modified and ' *' or ''}")
+            if change!=0.0:
+                self._logger.info(f"{i: 3d} {param!s} change={change:g}")
+            else:
+                self._logger.info(f"{i: 3d} {param!s}")
 
         self._ncall += 1
 
         ret = self._statistic.data[0]
 
-        if n_modified_pars<n_pars:
-            calctype = ": derivative"
-        else:
-            if self._ncall==1:
-                calctype = n_pars>1 and ": step/hessian" or ""
-            else:
-                calctype = n_pars>2 and ": step/hessian" or ""
-
-        modified_pars_numbers = list(map(str, modified_pars_numbers))
-        if n_modified_pars>len(modified_pars):
-            modified_pars.append("...")
-            modified_pars_numbers.append("...")
-
-        self._logger.info(f"Modified parameters {n_modified_pars}/{n_pars}{calctype}")
-        self._logger.info(f"Modified parameters: {', '.join(modified_pars)}")
-        self._logger.info(f"Modified parameters: {', '.join(modified_pars_numbers)}")
-        self._logger.info(f"Statistic {self._ncall}: {ret}")
+        self._logger.info(f"Modified parameters {modified_parameters_count}/{n_parameters} ({call_type}): {modified_parameter_numbers}; {modified_parameters}")
+        self._logger.info(f"Statistic {self._ncall} (step {self._nstep}): {ret}")
+        if call_type!="initial":
+            fcn_change = ret - self._previous_fcn_step
+            self._logger.info(f"Statistic change: {fcn_change:g}")
         self._logger.info("")
+
+        if not call_type=="derivative":
+            self._previous_fcn_step = ret
+            self._previous_parameters_step = values.copy()
 
         return ret
 
-    def __call__(self, values: "NDArray") -> float:
+    def __call__(self, values: NDArray) -> float:
         return self._function(values)
